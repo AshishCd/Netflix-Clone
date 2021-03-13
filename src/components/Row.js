913be64bcd700cb2a66b69fb449af60158a8120f
Ashish surve <asurve18@yahoo.com>
2021-03-13 11:19:02 +0530

    added firebase config and movie trailer

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "./Row.css";
-
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseUrl = "https://image.tmdb.org/t/p/original";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,6 +17,29 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     };
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    }
+  }
+
+  const handleClick = (movie) => {
+    if(trailerUrl){
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+      .then((url) => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
+  }
 
   return (
     <div className="row">
@@ -27,10 +52,12 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
               key={i.id}
               src={`${baseUrl}${isLargeRow ? i.poster_path : i.backdrop_path}`}
               alt={i.original_title}
+              onClick={() => handleClick(i)}
             />
           );
         })}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
